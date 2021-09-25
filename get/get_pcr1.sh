7fe18f30ab701464eeaccedb88234448c2692064
yeonghoon1006 <89287272+yeonghoon1006@users.noreply.github.com>
2021-09-25 09:16:41 +0900

    Create get_pcr1.sh

---
+++ get/get_pcr1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+sys='GURO_PCRF1'
+db='GR_PCRF1'
+sys2='PCRF1'
+day=`date +%Y%m%d -d '3 min ago'`
+min=`echo "$(date +%M)%5" | bc`
+HOST='***'
+USER='***'
+PASSWORD='***'
+
+
+if [ $min -gt 1 ]
+then
+        dis1=`echo "$(date +%M)%5" | bc`
+        dis2=`echo "$(date +%M)%5 + 5" | bc`
+else
+        dis1=`echo "$(date +%M)%5 + 5 " | bc`
+        dis2=`echo "$(date +%M)%5 + 10" | bc`
+fi
+
+ld='/home/get/'
+rd='/home/argos/5MIN/'$sys'/'$day
+
+sf=`date +'%Y%m%d.%H%M' -d ''$dis2' min ago'`
+tf=`date +'%Y%m%d.%H%M' -d ''$dis1' min ago'`
+ff='A'$sf'+0000_'$tf'+0000_'$sys'.xml'
+
+mdt=`date +'%Y-%m-%d %H:%M:00' -d ''$dis2' min ago'`
+
+echo 'Target File name : '$ff
+
+sshpass -p $PASSWORD sftp $USER@$HOST <<EOF
+
+cd $rd
+pwd
+
+#!mkdir $ld
+lcd $ld
+!pwd
+
+get $ff
+bye
+EOF
+
+echo $ld$ff
+python3 /home/parsing/crawling_pcrf.py $db /home/get/$ff
+
+rm -f /home/get/$ff
+
+python3 /home/maca/maca_lte.py $mdt $sys2
